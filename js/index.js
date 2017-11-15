943806da0823348300468baf2f8615512d43cc03
ym985 <yiz637@ucsd.edu>
2017-11-14 18:09:11 -0800

    add functionality to upload calendar

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ var user;
 window.onload = function(){
     email = localStorage.getItem('currentUser');
     user = JSON.parse(localStorage.getItem(email));
-    if (user.knee == "true") document.getElementsByClassName('squats').innerHTML = '';
-    if (user.back == "true") document.getElementsByClassName('yoga').innerHTML = '';
+    if (user.knee == "true") document.getElementsByClassName("squats").innerHTML = '';
+    if (user.back == "true") document.getElementsByClassName("yoga").innerHTML = '';
 }
