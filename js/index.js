3764a0e113584da87c60a91d669c997efb59fdcd
ym985 <yiz637@ucsd.edu>
2017-11-14 18:25:59 -0800

    add functionality to upload calendar

--- js/index.js
+++ js/index.js
@@ -5,11 +5,20 @@ window.onload = function(){
     user = JSON.parse(localStorage.getItem(email));
     calendar = document.getElementById('calendar');
     if (user.knee == "true") {
-        calendar.rows[1].cells[2].innerHTML = '';
-        calendar.rows[1].cells[4].innerHTML = '';
+        calendar.rows[1].cells[0].innerHTML = 'No Workout Today!';
+        calendar.rows[1].cells[2].innerHTML = 'No Workout Today!';
+        if(user.back == true) {
+            calendar.rows[1].cells[4].innerHTML = 'No Workout Today!';
+        }
+        else {
+            calendar.rows[1].cells[4].innerHTML = 'Squats';
+            calendar.rows[2].cells[4].innerHTML = '';
+        }
+
     }
     if (user.back == "true") {
         calendar.rows[2].cells[1].innerHTML = '';
         calendar.rows[2].cells[4].innerHTML = '';
     }
+
 }
